de1e88f8fbda5832f8d91dfa3a11fb373eb999b6
Leon <lw75251@gmail.com>
2018-11-07 05:29:50 -0800

    Changed to Fragments - must determine whether to use Activites, Fragments, or both!

--- Gravitate/app/src/main/java/com/example/ken/gravitate/MainActivity.java
+++ Gravitate/app/src/main/java/com/example/ken/gravitate/MainActivity.java
@@ -1,54 +1,53 @@
 package com.example.ken.gravitate;
 
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import  android.support.v4.widget.NestedScrollView;
+import android.widget.GridLayout;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
 
     private FloatingActionButton fab;
-    private NestedScrollView nestedScrollView;
-    private DrawerLayout drawerLayout;
     private ImageView profile;
     private View header;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
-        // Setting up Navigation Drawer
-        drawerLayout = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-            // Getting references to navDrawer items
-        header = navigationView.getHeaderView(0);
-        profile = header.findViewById(R.id.nav_profile);
-            //
-        if(profile == null){
-            startActivity(new Intent(MainActivity.this, NewEvent.class));
-        }
-
-            // Setting up the navigation drawer icon
+        // Toolbar Setup
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        ActionBar actionbar = getSupportActionBar();
-        actionbar.setDisplayHomeAsUpEnabled(true);
-        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
-            //
-        //
 
+        // Side-Navigation Setup
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // Side-Navigation Menu Setup
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Floating Action Button Setup
         fab = findViewById(R.id.fab);
 
         // Pressing on FAB, open new Activity -> New Event
@@ -59,21 +58,51 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // Pressing on Profile Pic, open new Activity -> My Profile
-        profile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, MyProfile.class));
-            }
-        });
-
     }
+
+    // Creates a Fragment Instance of respective menu item
+    // TODO: Determine whether to use Fragments or Activites
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch(menuItem.getItemId()){
+            case R.id.nav_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ProfileFragment()).commit();
+                break;
+        }
+        switch(menuItem.getItemId()){
+            case R.id.nav_messages:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new MessageFragment()).commit();
+                break;
+        }
+        switch(menuItem.getItemId()){
+            case R.id.nav_settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new SettingFragment()).commit();
+                break;
+        }
+
+        return true;
+    }
+
+    // When pressing back button, closes menu instead of exiting program
+    @Override
+    public void onBackPressed() {
+        if( drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+
     // Open NavDrawer when MenuButton is pressed
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
         switch(item.getItemId()){
             case android.R.id.home:
-                drawerLayout.openDrawer(GravityCompat.START);
+                drawer.openDrawer(GravityCompat.START);
                 return true;
         }
         return super.onOptionsItemSelected(item);
