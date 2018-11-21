5b0c27a047100ebbb3df424a3a6c6bfa44c99894
Leon <lw75251@gmail.com>
2018-11-21 08:25:47 -0800

    Google Sign-in Accounts appears in Firebase

--- Gravitate/app/src/main/java/com/example/ken/gravitate/LoginActivity.java
+++ Gravitate/app/src/main/java/com/example/ken/gravitate/LoginActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -17,46 +19,74 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
 
-    SignInButton button;
+    SignInButton sign_in_bttn;
+    Button sign_out_bttn;
     FirebaseAuth mAuth;
     private final static int RC_SIGN_IN = 2; // Request Code for starting new activity
     GoogleSignInClient mGoogleSignInClient;
     FirebaseAuth.AuthStateListener mAuthListener;
-    boolean skipLoginPage = true; // skip the login page
+    private static final String TAG = "GoogleActivity";
+    private static final String web_client_id = "1070051773756-o6l5r1l6v7m079r1oua2lo0rsfeu8m9i.apps.googleusercontent.com";
 
     @Override
     protected void onStart() {
         super.onStart();
+
+        FirebaseUser curUser = mAuth.getCurrentUser();
+        // Moves to Main Page if User is already logged in
+        if ( curUser != null ) {
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+        }
 
         mAuth.addAuthStateListener(mAuthListener);
     }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        if(skipLoginPage){
-            startActivity(new Intent(LoginActivity.this, MainActivity.class));
-        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         // Configure sign-in to request the user's ID, email address, and basic profile.
         // ID and basic profile are included in DEFAULT_SIGN_IN.
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(web_client_id)
                 .requestEmail()
                 .build();
 
         // Build a GoogleSignInClient with the options specified by gso.
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        mAuth = FirebaseAuth.getInstance();
 
-        // Gets Button Reference in Login_Activity Layout
-        button = (SignInButton) findViewById(R.id.googleBtn);
-        button.setOnClickListener(new View.OnClickListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                // We have a user
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if ( user != null ) {
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                }
+            }
+        };
 
+        // Floating Action Button Setup
+        sign_in_bttn = findViewById(R.id.googleBtn);
+        sign_out_bttn = findViewById(R.id.signout_bttn);
+
+        // Gets Google Button Reference in Login_Activity Layout
+        sign_in_bttn.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
                 switch (v.getId()) {
                     case R.id.googleBtn:
@@ -68,24 +98,17 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        mAuth = FirebaseAuth.getInstance();
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
+        sign_out_bttn.setOnClickListener( new View.OnClickListener() {
             @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                // We have a user
-                if ( firebaseAuth.getCurrentUser() != null ) {
-                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            public void onClick(View v) {
+                switch(v.getId()) {
+                    case R.id.signout_bttn:
+                    signOut();
+                    break;
                 }
             }
-        };
+        });
 
-    }
-
-
-
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
     @Override
@@ -104,19 +127,54 @@ public class LoginActivity extends AppCompatActivity {
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-
+            firebaseAuthWithGoogle(account);
             // Signed in successfully, show authenticated UI.
-//            updateUI(account);
+            // updateUI(account);
         } catch (ApiException e) {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            // TODO: Need Tag Codes
-            Log.w("Google-Sign-In", "signInResult:failed code=" + e.getStatusCode());
-//            updateUI(null);
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            // updateUI(null);
         }
     }
 
-/*    private void updateUI(GoogleSignInAccount account) {
-        startActivity(new Intent(LoginActivity.this, MainActivity.class));
-    }*/
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            // updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Snackbar.make(findViewById(R.id.login_layout), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                            // updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void signOut() {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // ...
+                    }
+                });
+    }
 }
