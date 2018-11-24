4391b5d2d8992535b82028386c120a2ef2d58572
KenLHua <khuarice@gmail.com>
2018-11-23 17:22:37 -0800

    Added prototype dynamic flight form

--- app/src/main/java/com/example/ken/gravitate/Event/InputFlight.java
+++ app/src/main/java/com/example/ken/gravitate/Event/InputFlight.java
@@ -1,18 +1,62 @@
 package com.example.ken.gravitate.Event;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 
 import com.example.ken.gravitate.R;
 
 public class InputFlight extends AppCompatActivity {
+    private RadioGroup inputGroup;
+    private RadioButton flightRadio;
+    private RadioButton manualRadio;
+    private TextInputLayout flightNumberEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.new_event_layout);
+        setContentView(R.layout.input_flight_information);
         Toolbar toolbar = findViewById(R.id.toolbar);
-        toolbar.setTitle(R.string.new_event_toolbar);
+        toolbar.setTitle(R.string.input_flight_toolbar);
         setSupportActionBar(toolbar);
+
+        // Creating input TextFields
+
+
+        // Setting Radio Buttons
+        inputGroup = (RadioGroup) findViewById(R.id.flightRadioGroup);
+        flightRadio = (RadioButton) findViewById(R.id.flightRadio);
+        manualRadio = (RadioButton) findViewById(R.id.manualRadio);
+        flightNumberEditText = (TextInputLayout) findViewById(R.id.eventLocationLayout);
+
+
+        // Setting Radio hide/show behavior
+        flightRadio.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(flightRadio.isChecked()){
+                    flightNumberEditText.setVisibility(View.GONE);
+                }
+                else {
+                    flightNumberEditText.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        inputGroup.check(R.id.flightRadio);
+    }
+    // **Incomplete** Add Checkmark to the ActionBar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.actionbar_menu, menu);
+        return true;
     }
 }
