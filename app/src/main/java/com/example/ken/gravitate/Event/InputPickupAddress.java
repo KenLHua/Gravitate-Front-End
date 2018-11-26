ac80137516ea80b200e8b3d6b256665635832309
KenLHua <32448050+KenLHua@users.noreply.github.com>
2018-11-26 00:44:52 -0800

    Got Input Pickup Address textbox to work correctly

--- app/src/main/java/com/example/ken/gravitate/Event/InputPickupAddress.java
+++ app/src/main/java/com/example/ken/gravitate/Event/InputPickupAddress.java
@@ -3,6 +3,8 @@ package com.example.ken.gravitate.Event;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 //Necessary libraries for Address Autocomplete functionality
@@ -21,12 +23,15 @@ public class InputPickupAddress extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("InputPickupAddress");
         setSupportActionBar(toolbar);
-        final TextView txtVw = findViewById(R.id.placeName);
+
+        final TextView inputPickup = findViewById(R.id.inputPickup);
+        final ImageButton pickupClear = findViewById(R.id.clear_pickup_button);
+
 
         //Note: Address Autocomplete sorts address priority by user location/proximity.
 
         //Initialize Address Autocomplete Fragment
-        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+        final PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
                 getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
 
         //Limit search to addresses in United States only, without the filter the autocomplete will
@@ -35,18 +40,26 @@ public class InputPickupAddress extends AppCompatActivity {
                 .setCountry("us")
                 .build();
         autocompleteFragment.setFilter(filter);
+        autocompleteFragment.setHint("Pickup Address");
+        pickupClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inputPickup.setText("");
+            }
+        });
 
         //Listener to store selected address and display on screen
         autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
             public void onPlaceSelected(Place place) {
-                txtVw.setText("Pick-Up Address: \n"+place.getName());
+                inputPickup.setText(place.getAddress());
             }
             @Override
             public void onError(Status status) {
-                txtVw.setText(status.toString());
+                inputPickup.setText(status.toString());
             }
         });
+
     }
 
 
