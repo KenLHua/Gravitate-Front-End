2cd12d5f3205a2382058aea832f65052275da768
Jason <jdchau@ucsd.edu>
2018-11-26 22:07:37 -0800

    got Edit Account Info to work

--- app/src/main/java/com/example/ken/gravitate/Settings/SettingsActivity.java
+++ app/src/main/java/com/example/ken/gravitate/Settings/SettingsActivity.java
@@ -1,12 +1,19 @@
 package com.example.ken.gravitate.Settings;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toolbar;
-
+import com.example.ken.gravitate.Account.LoginActivity;
+import com.example.ken.gravitate.Event.ScheduledEvents;
 import com.example.ken.gravitate.R;
 
 public class SettingsActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
