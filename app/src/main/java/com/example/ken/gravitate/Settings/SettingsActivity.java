f98f4c9b80f54d6cdb0e22e979ee269a5eed5b90
KenLHua <32448050+KenLHua@users.noreply.github.com>
2018-11-28 01:34:28 -0800

    fixed some merging issues

--- app/src/main/java/com/example/ken/gravitate/Settings/SettingsActivity.java
+++ app/src/main/java/com/example/ken/gravitate/Settings/SettingsActivity.java
@@ -1,10 +1,7 @@
 package com.example.ken.gravitate.Settings;
 
-<<<<<<< HEAD
 import android.content.Context;
-=======
 import android.content.Intent;
->>>>>>> MaterialSettings
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.Preference;
@@ -12,12 +9,9 @@ import android.support.v7.preference.PreferenceFragmentCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toolbar;
-<<<<<<< HEAD
-=======
 import com.example.ken.gravitate.Account.LoginActivity;
 import com.example.ken.gravitate.Event.ScheduledEvents;
 import com.example.ken.gravitate.R;
->>>>>>> MaterialSettings
 
 public class SettingsActivity extends AppCompatActivity {
 
