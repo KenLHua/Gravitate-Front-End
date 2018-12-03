965678db4f623fe9d323c906d812bbfa5439d721
Jason <jdchau@ucsd.edu>
2018-12-02 17:41:07 -0800

    added toolbar and back button to Settings page

--- app/src/main/java/com/example/ken/gravitate/Settings/SettingsActivity.java
+++ app/src/main/java/com/example/ken/gravitate/Settings/SettingsActivity.java
@@ -11,20 +11,14 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD
 
         setContentView(R.layout.settings);
-        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
-        toolbar.setTitle("Settings");
-        toolbar.setNavigationIcon(R.drawable.system_icon_back);
 
         // Back button will go to previous page when clicked on
-=======
         setContentView(R.layout.settings);
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Settings");
         toolbar.setNavigationIcon(R.drawable.system_icon_back);
->>>>>>> f766ca5d0cc50586c4038b5d9a87ad294ecae8ca
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,16 +27,10 @@ public class SettingsActivity extends AppCompatActivity {
         });
         setSupportActionBar(toolbar);
 
-<<<<<<< HEAD
 
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.settings_fragment, new SettingsFragment())
-=======
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.setting_fragment, new SettingsFragment())
->>>>>>> f766ca5d0cc50586c4038b5d9a87ad294ecae8ca
                 .commit();
     }
 
