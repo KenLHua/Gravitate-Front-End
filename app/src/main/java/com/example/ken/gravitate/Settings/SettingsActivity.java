1dd8444589f5f86ba94d35a802c80d5b40eadd4c
Jason <jdchau@ucsd.edu>
2018-12-02 17:25:21 -0800

    added toolbar and back button to Settings page

--- app/src/main/java/com/example/ken/gravitate/Settings/SettingsActivity.java
+++ app/src/main/java/com/example/ken/gravitate/Settings/SettingsActivity.java
@@ -18,9 +18,32 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.settings);
+        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Settings");
+        toolbar.setNavigationIcon(R.drawable.system_icon_back);
+
+        // Back button will go to previous page when clicked on
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSupportNavigateUp();
+            }
+        });
+        setSupportActionBar(toolbar);
+
+
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
+                .replace(R.id.settings_fragment, new SettingsFragment())
                 .commit();
+    }
+
+    // Helper method for back button
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
